063b01456bc21f638464557954d0f0bf67f267a9
Ascf <malykhinxv@gmail.com>
2025-06-24 11:03:28 +0200

    Edit the notifier message

--- notifier/formatter.py
+++ notifier/formatter.py
@@ -3,23 +3,13 @@ from utils.time import ago
 
 
 def format_signal_detailed(signal: SetupSignal) -> str:
-    tf_labels = {
-        "1d": "1D",
-        "4h": "4H",
-        "1h": "1H",
-        "15m": "15M"
-    }
-
     level_emojis = {
         "low": "🌘",
         "medium": "🌓",
         "high": "🌖"
     }
 
-    tf_lines = "\n".join([
-        f"{tf_labels[tf].lower()} {signal.direction}"
-        for tf in signal.confirmed_timeframes
-    ])
+    tf_lines = "\n".join([f"{tf} {signal.direction}" for tf in signal.confirmed_timeframes])
 
     text = (
         f"{level_emojis[signal.confidence]} *{signal.symbol}* {signal.direction.upper()}\n\n"
