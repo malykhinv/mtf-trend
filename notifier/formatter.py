5e75e0c2b3a23febfdc980c67f814de34db06fab
Ascf <malykhinxv@gmail.com>
2025-06-24 12:06:46 +0200

    Edit message

--- notifier/formatter.py
+++ notifier/formatter.py
@@ -1,20 +1,18 @@
 from domain.models.setup_signal import SetupSignal
-from utils.time import ago
 
 
-def format_signal_detailed(signal: SetupSignal) -> str:
-    level_emojis = {
-        "low": "🌘",
+def format_message(signal: SetupSignal) -> str:
+    emoji_map = {
+        "low": "🌑",
         "medium": "🌓",
-        "high": "🌖"
+        "high": "🌕"
     }
 
-    tf_lines = "\n".join([f"{tf} {signal.direction}" for tf in signal.confirmed_timeframes])
+    emoji = emoji_map.get(signal.confidence, "🌑")
+    tf_block = "\n".join(f"{tf} {signal.direction}" for tf in signal.confirmed_timeframes)
 
-    text = (
-        f"{level_emojis[signal.confidence]} *{signal.symbol}* {signal.direction.upper()}\n\n"
-        f"{tf_lines}\n\n"
-        f"1 : {signal.rr}"
+    return (
+        f"{emoji} *{signal.symbol}*\n\n"
+        f"1:{signal.rr} {signal.direction.upper()}\n"
+        f"`{tf_block}`"
     )
-
-    return text
