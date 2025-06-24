0c275d1614a588367cc1be7268888c9103dfaa09
Ascf <malykhinxv@gmail.com>
2025-06-24 20:26:22 +0200

    Improve logs

--- utils/logger.py
+++ utils/logger.py
@@ -1,6 +1,11 @@
 # utils/logger.py
-from datetime import datetime
+import logging
+
+logging.basicConfig(
+    format="%(asctime)s %(message)s",
+    datefmt="%H:%M:%S",
+    level=logging.INFO
+)
 
 def log(message: str) -> None:
-    now = datetime.now().strftime("%H:%M:%S")
-    print(f"{now} {message}")
+    logging.info(message)
