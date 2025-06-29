699275a1416c9b8d167003ab22b4e4c2407be220
Ascf <malykhinxv@gmail.com>
2025-06-29 14:47:04 +0200

    Change Scenario values

--- domain/models/scenario.py
+++ domain/models/scenario.py
@@ -2,7 +2,7 @@ from enum import Enum
 
 
 class Scenario(str, Enum):
-    FLAT_BOUNCE = "flat_bounce"
-    FLAT_FAKE_BREAKOUT = "flat_fake_breakout"
-    MOMENTUM_INITIATION = "momentum_initiation"
-    MOMENTUM_PULLBACK = "momentum_pullback"
+    FLAT_BOUNCE = "Flat bounce"
+    FLAT_FAKE_BREAKOUT = "Flat fake breakout"
+    MOMENTUM_INITIATION = "Momentum initiation"
+    MOMENTUM_PULLBACK = "Momentum pullback"
