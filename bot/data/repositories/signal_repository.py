cdfcfb335963b38a52123d960e05701bcb6cb0e1
Vladislav <73940862+malykhinv@users.noreply.github.com>
2025-09-26 12:37:00 +0200

    Remove unused SignalRepository read methods

--- bot/data/repositories/signal_repository.py
+++ bot/data/repositories/signal_repository.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from threading import RLock
-from typing import Dict, List, Optional
+from typing import Dict
 
 from ...domain.models.entities import Signal
 from ..io.storage import Storage
@@ -18,14 +18,6 @@ class SignalRepository:
         with self._lock:
             self._cache[signal.id] = signal
             self._storage.save_signal(signal)
-
-    def get(self, signal_id: str) -> Optional[Signal]:
-        with self._lock:
-            return self._cache.get(signal_id)
-
-    def all(self) -> List[Signal]:
-        with self._lock:
-            return list(self._cache.values())
 
     def load(self) -> None:
         with self._lock:
