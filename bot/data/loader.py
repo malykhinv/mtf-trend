f1add0937d1d87a1f79f30d73e351f6ce00ba4c9
Ascf <malykhinxv@gmail.com>
2025-09-30 19:12:08 +0200

    Fix _fetch_ohlcv_with_retry

--- bot/data/loader.py
+++ bot/data/loader.py
@@ -325,7 +325,7 @@ class CcxtMarketDataLoader(MarketDataLoader):
                     timeframe,
                     exc,
                 )
-                return []
+                raise
 
     def load(self, request: HistoricalRequest) -> Iterable[Bar]:
         client = self._clients.get(request.exchange)
