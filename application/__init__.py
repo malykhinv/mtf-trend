a77c50ae0aff9e4eb08161dc6ffaca3455f47d05
Vladislav <73940862+malykhinv@users.noreply.github.com>
2025-10-11 20:54:07 +0200

    Remove Noop trading adapter

--- application/__init__.py
+++ application/__init__.py
@@ -1,9 +1,7 @@
 from .runtime import FeedMonitor, GUARDS, RuntimeGuards
-from .trading import NoopTradingAdapter
 
 __all__ = [
     "FeedMonitor",
     "GUARDS",
     "RuntimeGuards",
-    "NoopTradingAdapter",
 ]
