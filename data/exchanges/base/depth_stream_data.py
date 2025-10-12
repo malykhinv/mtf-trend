9e25bf784126a32b83b40892b620de639e135178
Ascf <malykhinxv@gmail.com>
2025-10-12 12:39:32 +0200

    Erase the data layer

--- data/exchanges/base/depth_stream_data.py
+++
@@ -1,7 +0,0 @@
-from typing import Union
-
-from domain.models import OrderBookSnapshot, OrderBookUpdate
-
-DepthStreamData = Union[OrderBookSnapshot, OrderBookUpdate]
-
-__all__ = ["DepthStreamData"]
