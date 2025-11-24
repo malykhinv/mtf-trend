c9dfc8bb565205e50684a19ce62eb340421ebdcd
Ascf <malykhinxv@gmail.com>
2025-11-24 18:48:16 +0100

    Fix _evaluate_buy

--- crypto_screener/execution/run_test_market.py
+++ crypto_screener/execution/run_test_market.py
@@ -50,7 +50,7 @@ def _evaluate_buy(
         setup: Buy,
         future_bars: list[Bar]
 ) -> Optional[tuple[TradeResult, float]]:
-    entry_price = setup.breakeven_price + cfg.TEST_SLIPPAGE_PCT
+    entry_price = setup.breakeven_price * (1 + cfg.TEST_SLIPPAGE_PCT/100)
     stop_loss_price = setup.stop_loss_price
     take_profit_price = setup.take_profit_price
     partial_close_price = setup.partial_close_price
