148b7d2d441cddc633d531b8c1a354e1402fb46b
Vladislav <73940862+malykhinv@users.noreply.github.com>
2025-12-12 10:36:58 +0100

    Refactor live runner task scheduling

--- crypto_screener/domain/models/scheduled_task.py
+++ crypto_screener/domain/models/scheduled_task.py
@@ -8,5 +8,6 @@ from crypto_screener.domain.models.timeframe import Timeframe
 @dataclass(order=True)
 class ScheduledTask:
     next_run_at: datetime
+    priority: int = field(default=0)
     symbol: FuturesSymbol = field(compare=False)
     timeframe: Timeframe = field(compare=False)
