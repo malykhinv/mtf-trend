e099f872e2ffeb5f81c9776f1d2dd4bd3fcdd173
Vladislav <73940862+malykhinv@users.noreply.github.com>
2025-12-15 07:46:40 +0100

    Refactor plot plotting themes and dispatcher

--- crypto_screener/config/__init__.py
+++ crypto_screener/config/__init__.py
@@ -1,5 +1,15 @@
 from crypto_screener.config.app_config import AppConfig, app_cfg
+from crypto_screener.config.plot_theme import PlotTheme, ppo_plot_theme
 from crypto_screener.config.ppo_config import PpoConfig, ppo_cfg
 from crypto_screener.config.symbol_config import SymbolConfig, symbol_cfg
 
-__all__ = ["AppConfig", "PpoConfig", "SymbolConfig", "app_cfg", "ppo_cfg", "symbol_cfg"]
+__all__ = [
+    "AppConfig",
+    "PpoConfig",
+    "SymbolConfig",
+    "PlotTheme",
+    "app_cfg",
+    "ppo_cfg",
+    "symbol_cfg",
+    "ppo_plot_theme",
+]
