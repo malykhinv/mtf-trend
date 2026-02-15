62fe230212b99bd1e2150343767fb307ebae0bc4
Vladislav <73940862+malykhinv@users.noreply.github.com>
2026-02-15 19:05:26 +0100

    Remove deprecated TimeAlignment quality component

--- data/quality/__init__.py
+++ data/quality/__init__.py
@@ -3,6 +3,5 @@
 from data.quality.data_validator import DataValidator
 from data.quality.deduplicator import Deduplicator
 from data.quality.gap_detector import GapDetector
-from data.quality.time_alignment import TimeAlignment
 
-__all__ = ["DataValidator", "Deduplicator", "GapDetector", "TimeAlignment"]
+__all__ = ["DataValidator", "Deduplicator", "GapDetector"]
