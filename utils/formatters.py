f671be35c8043ef2e62dd997bf460775f07b88ce
Vladislav <73940862+malykhinv@users.noreply.github.com>
2026-02-15 18:51:56 +0100

    Remove datetime human formatter utility

--- utils/formatters.py
+++ utils/formatters.py
@@ -1,16 +1,3 @@
-"""Вспомогательные функции форматирования даты и времени."""
+"""Вспомогательные форматтеры без timezone и без datetime-нормализаций."""
 
 from __future__ import annotations
-
-from datetime import UTC, datetime
-
-
-def format_datetime_human(timestamp_ms: int) -> str:
-    """Форматирует unix timestamp в миллисекундах в человекочитаемый UTC вид."""
-    if not isinstance(timestamp_ms, int):
-        msg = "Timestamp must be int in unix milliseconds."
-        raise TypeError(msg)
-    if timestamp_ms < 0:
-        msg = "Timestamp must be non-negative."
-        raise ValueError(msg)
-    return datetime.fromtimestamp(timestamp_ms / 1000, tz=UTC).strftime("%d.%m.%Y %H:%M:%S")
