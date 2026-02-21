c21981c65d0c2d61830b600593a0951680dbbcb8
Vladislav <73940862+malykhinv@users.noreply.github.com>
2026-02-21 19:22:43 +0100

    Add standalone bee_bite FSM engine

--- strategy/bee_bite/bee_bite_strategy.py
+++ strategy/bee_bite/bee_bite_strategy.py
@@ -1,4 +1,4 @@
-"""Адаптер стратегии bee_bite поверх breakout-ядра."""
+"""Стратегия bee_bite поверх собственного FSM-движка."""
 
 from __future__ import annotations
 
@@ -10,92 +10,28 @@ from strategy.bee_bite.config import (
     build_bee_bite_grid,
     validate_bee_bite_params,
 )
-from strategy.breakout.breakout_strategy import BreakoutStrategy
-from strategy.breakout.config import BreakoutParams
+from strategy.bee_bite.engine import BeeBiteEngine
 from vectorbt_runner.mtf_frames import SymbolMtfFrames
 
 
 class BeeBiteStrategy(BaseStrategy[BeeBiteParams]):
-    """bee_bite использует breakout-логику с собственными именами параметров."""
-
-    def __init__(
-        self,
-        breakout: BreakoutStrategy,
-        *,
-        profile_id: BeeBiteProfileId,
-        grid_mode: BeeBiteGridMode,
-    ) -> None:
-        self._breakout = breakout
+    def __init__(self, *, profile_id: BeeBiteProfileId, grid_mode: BeeBiteGridMode) -> None:
         self._profile_id = profile_id
         self._grid_mode = grid_mode
-
-    def _to_breakout_params(self, params: BeeBiteParams) -> BreakoutParams:
-        return BreakoutParams(
-            lookback=params.bite_lookback,
-            volume_mult=params.bite_volume_mult,
-            retest_window_hours=params.bite_retest_window_hours,
-            retest_zone=params.bite_retest_zone,
-            min_rr=params.bite_min_rr,
-            sl_mode=params.bite_sl_mode,
-            tp2_mult=params.bite_tp2_mult,
-            min_body_ratio=params.bite_min_body_ratio,
-            min_move_atr=params.bite_min_move_atr,
-            max_retest_depth=params.bite_max_retest_depth,
-            confirmation_bars=params.bite_confirmation_bars,
-            entry_trigger=params.bite_entry_trigger,
-            symbol=params.symbol,
-            retest_zone_atr=params.bite_retest_zone_atr,
-            levels_timeframe=params.levels_timeframe,
-            entry_timeframe=params.entry_timeframe,
-            r_trade=params.bite_r_trade,
-            portfolio_risk_limit=params.bite_portfolio_risk_limit,
-            min_stop_atr_ratio=params.bite_min_stop_atr_ratio,
-            t_max_in_trade=params.bite_t_max_in_trade,
-        )
-
-    @staticmethod
-    def _from_breakout_params(params: BreakoutParams) -> BeeBiteParams:
-        return BeeBiteParams(
-            bite_lookback=params.lookback,
-            bite_volume_mult=params.volume_mult,
-            bite_retest_window_hours=params.retest_window_hours,
-            bite_retest_zone=params.retest_zone,
-            bite_min_rr=params.min_rr,
-            bite_sl_mode=params.sl_mode,
-            bite_tp2_mult=params.tp2_mult,
-            bite_min_body_ratio=params.min_body_ratio,
-            bite_min_move_atr=params.min_move_atr,
-            bite_max_retest_depth=params.max_retest_depth,
-            bite_confirmation_bars=params.confirmation_bars,
-            bite_entry_trigger=params.entry_trigger,
-            symbol=params.symbol,
-            bite_retest_zone_atr=params.retest_zone_atr,
-            levels_timeframe=params.levels_timeframe,
-            entry_timeframe=params.entry_timeframe,
-            bite_r_trade=params.r_trade,
-            bite_portfolio_risk_limit=params.portfolio_risk_limit,
-            bite_min_stop_atr_ratio=params.min_stop_atr_ratio,
-            bite_t_max_in_trade=params.t_max_in_trade,
-        )
+        self._engine = BeeBiteEngine()
 
     def validate_config(self, params: BeeBiteParams) -> None:
         validate_bee_bite_params(params)
-        self._breakout.validate_config(self._to_breakout_params(params))
+        self._engine.validate_config(params)
 
     def prepare_data(self, data):
-        return self._breakout.prepare_data(data)
+        return self._engine.prepare_data(data)
 
     def generate_events(self, data, params: BeeBiteParams):
-        return self._breakout.generate_events(data, self._to_breakout_params(params))
+        return self._engine.generate_events(data, params)
 
     def generate_events_multi_tf(self, *, mtf_frames: SymbolMtfFrames, params: BeeBiteParams):
-        return self._breakout.generate_events_multi_tf(mtf_frames=mtf_frames, params=self._to_breakout_params(params))
-
-    def generate_events_portfolio(self, *, symbol_frames: dict[str, SymbolMtfFrames], params: BeeBiteParams):
-        return self._breakout.generate_events_portfolio(
-            symbol_frames=symbol_frames,
-            params=self._to_breakout_params(params),
-        )
+        return self._engine.generate_events_multi_tf(mtf_frames=mtf_frames, params=params)
 
     def build_parameter_grid(self) -> list[BeeBiteParams]:
         return build_bee_bite_grid(profile_id=self._profile_id, grid_mode=self._grid_mode)
@@ -124,11 +60,7 @@ class BeeBiteStrategy(BaseStrategy[BeeBiteParams]):
         }
 
     def prepare_symbol_context(self, *, symbol: str, mtf_frames: SymbolMtfFrames, params: BeeBiteParams):
-        return self._breakout.prepare_symbol_context(
-            symbol=symbol,
-            mtf_frames=mtf_frames,
-            params=self._to_breakout_params(params),
-        )
+        return None
 
     def consume_last_generation_diagnostics(self) -> dict[str, object]:
-        return self._breakout.consume_last_generation_diagnostics()
+        return self._engine.consume_last_generation_diagnostics()
